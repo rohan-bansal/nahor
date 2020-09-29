ed7a9064d90c65edb2733b9414da8944b51f58bd
Rohan Bansal <bansalrohan18@gmail.com>
2020-09-28 20:09:14 -0700

    fix passenger? v2

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,7 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
+
+INTERP = os.path.join(os.getcwd(), '/virtualenv/repositories/nahor_cf/3.7/bin/python')
+sys.path.append(INTERP)
 
 
 from nahor import app as application
