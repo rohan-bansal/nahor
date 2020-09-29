0b2ee7910005ec9cb67ea6b4db046f1f6c56a723
Rohan Bansal <bansalrohan18@gmail.com>
2020-09-28 20:18:29 -0700

    wrong import

--- nahor/__init__.py
+++ nahor/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy, create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 # db = SQLAlchemy(app)
