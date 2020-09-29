6d06c29f1ea09142206ed9ffff9d381b915de6be
Rohan Bansal <bansalrohan18@gmail.com>
2020-09-28 19:41:18 -0700

    fix shortener unnecessary data

--- nahor/__init__.py
+++ nahor/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+# app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
