277e8d527a18f7bb8da9c878ba5316ccc19bae68
Rohan Bansal <bansalrohan18@gmail.com>
2020-09-30 08:44:22 -0700

    remove

--- nahor/__init__.py
+++ nahor/__init__.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-# db = create_engine("mysql://rohanban_rohan:s1lma549x@nahor.cf:3306/rohanban_nahorcf")
 
-# app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
